c6f74789e4e18c1bd8ae37268638aa3e35b9444b
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-08-24 08:28:07 +0200

    take input, use hypotenuse calc function

--- Homeworks/2_homework/hypotenuse_calculator.py
+++ Homeworks/2_homework/hypotenuse_calculator.py
@@ -19,3 +19,9 @@ import math
 def calculate_hypotenuse(a_len, b_len):
     return math.sqrt(math.pow(a_len, 2) + math.pow(b_len, 2))
 
+
+a = float(input("Podaj długość boku a: "))
+b = float(input("Podaj długość boku b: "))
+
+c = calculate_hypotenuse(a, b)
+print(f"Przy długości boków: a = {a:.2f} i b = {b:.2f}, c = {c:.2f}")
