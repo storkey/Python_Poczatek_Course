6960165acad517bc258e76b67f68c64ef0f655cc
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-08-29 00:53:47 +0200

    called calculate_investment and print results

--- Homeworks/3_homework/start_investment_calc.py
+++ Homeworks/3_homework/start_investment_calc.py
@@ -19,3 +19,8 @@ import calculations.investment_calculator
 initial_capital = float(input("Podaj wartość kapitału początkowego: "))
 percentage = float(input("Podaj oprocentowanie lokaty: "))
 years = float(input("Podaj na ile lat jest lokata: "))
+
+investment_value = calculations.investment_calculator.calculate_investment(initial_capital, percentage, years)
+
+print(f"Twoja lokata, przy kapitale początkowym: {initial_capital:.2f} zł, oprocentowaniu: {percentage:.2f}%"
+      f", zawarta na: {years:.2f} lat, da końcowo: {investment_value:.2f} zł")
