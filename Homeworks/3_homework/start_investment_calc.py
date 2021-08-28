68e299bb8bcb271285947a119007f499e77a5bb1
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-08-29 00:27:36 +0200

    added the content of the task

---
+++ Homeworks/3_homework/start_investment_calc.py
@@ -0,0 +1,17 @@
+"""
+Zadanie nr 3
+
+Napisz kalkulator obliczający wartość lokaty po pewnym czasie.
+
+Wczytaj od użytkownika informacje o:
+
+    Początkowym kapitale (wpłaconej kwocie)
+    Oprocentowaniu
+    Okresie trwania inwestycji (w latach)
+
+Umieść funkcję obliczająca wartość lokaty w pakiecie calculations,
+a wczytanie danych i uruchomienie obliczeń w pliku powyżej pakietu.
+
+Skorzystaj ze wzoru: wartość = wartość pocz. * (1 + procent/100) ^ liczba lat
+"""
+
