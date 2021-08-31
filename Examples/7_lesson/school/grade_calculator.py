4157e8588235299cab4d78639ab62bc9130d90dc
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-08-31 04:13:23 +0200

    implemented calculate_final_grade

--- Examples/7_lesson/school/grade_calculator.py
+++ Examples/7_lesson/school/grade_calculator.py
@@ -1,2 +1,9 @@
 from students_data import students, find_student_by_name
 
+
+def calculate_final_grade(all_grades):
+    avg = sum(all_grades) / len(all_grades)
+    final_grade = int(avg + 0.5)
+    return final_grade
+
+
