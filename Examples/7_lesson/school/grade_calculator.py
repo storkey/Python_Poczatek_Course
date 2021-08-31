6275f368401736c48ce55c26b76e22ef537f4b10
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-08-31 03:43:34 +0200

    created students in students_data.py

---
+++ Examples/7_lesson/school/grade_calculator.py
@@ -0,0 +1,36 @@
+students = [
+    {
+        "name": "Piotr",
+        "subjects_results": [
+            {
+                "subject": "matematyka",
+                "grades": [2, 3, 4, 5, 2, 3.5]
+            },
+            {
+                "subject": "historia",
+                "grades": [5, 5, 5, 5, 4, 5]
+            },
+            {
+                "subject": "fizyka",
+                "grades": [3, 3, 2, 3, 4, 3]
+            }
+        ]
+    },
+    {
+        "name": "Joanna",
+        "subjects_results": [
+            {
+                "subject": "matematyka",
+                "grades": [4, 3, 4, 5, 3, 4]
+            },
+            {
+                "subject": "historia",
+                "grades": [4, 4, 3, 2, 2, 3]
+            },
+            {
+                "subject": "fizyka",
+                "grades": [3.5, 4, 5, 4, 4, 4.5]
+            }
+        ]
+    }
+]
