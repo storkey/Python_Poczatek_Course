f7d4251796fcdd8083216e941e69a2d69beba161
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-08-31 03:55:04 +0200

    implemented is_student_in_school

--- Examples/7_lesson/school/grade_calculator.py
+++ Examples/7_lesson/school/grade_calculator.py
@@ -40,3 +40,9 @@ def find_student_by_name(name):
     for student in students:
         if student["name"] == name:
             return student
+
+
+def is_student_in_school(name):
+    if not find_student_by_name(name):
+        return False
+    return True
