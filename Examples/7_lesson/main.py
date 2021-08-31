9bff59ba17f3476c5e96cd3230891bbb6bdf1e53
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-08-31 04:27:31 +0200

    added welcome text, took input, check if in school

--- Examples/7_lesson/main.py
+++ Examples/7_lesson/main.py
@@ -1,2 +1,9 @@
 from school.students_data import is_student_in_school
 
+print("Witaj w elektronicznym dzienniku!")
+
+student_name = input("Podaj imię ucznia, żeby dowiedzieć się czy zdał do następnej klasy: ")
+
+if not is_student_in_school(student_name):
+    print(f"Niestety, {student_name} nie ma na liście...")
+    
