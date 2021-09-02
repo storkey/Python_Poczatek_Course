be2d875b7cc9d4480f72221a24d7173973ab1146
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-09-03 01:34:12 +0200

    ended create_order function wit updated quantity

--- Homeworks/different_ways_of_import/shop/order.py
+++ Homeworks/different_ways_of_import/shop/order.py
@@ -1,11 +1,19 @@
-from shop.products import products
+from shop.products import products, update_product_quantity
 
 
 def create_order(ordered_product, ordered_quantity):
-    order = {"product_name": ordered_product, "quantity": ordered_quantity}
     price = products[ordered_product]["price"]
     available_quantity = products[ordered_quantity]["quantity"]
 
     if available_quantity < ordered_quantity:
         print("Nie mamy tyle towaru")
         return None
+
+    total_price = ordered_quantity * price
+    order = {
+        "product": ordered_product,
+        "quantity": ordered_quantity,
+        "total_price": total_price
+    }
+    update_product_quantity(ordered_product, ordered_quantity)
+    return order
