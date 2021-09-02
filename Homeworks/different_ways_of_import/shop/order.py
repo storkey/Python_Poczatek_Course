4377c7b6fb460ccef8c0f05e66c96cc61b4730de
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-09-03 01:27:45 +0200

    price, quantity check in create_order

--- Homeworks/different_ways_of_import/shop/order.py
+++ Homeworks/different_ways_of_import/shop/order.py
@@ -1,3 +1,11 @@
+from shop.products import products
+
+
 def create_order(ordered_product, ordered_quantity):
     order = {"product_name": ordered_product, "quantity": ordered_quantity}
-    return order
+    price = products[ordered_product]["price"]
+    available_quantity = products[ordered_quantity]["quantity"]
+
+    if available_quantity < ordered_quantity:
+        print("Nie mamy tyle towaru")
+        return None
