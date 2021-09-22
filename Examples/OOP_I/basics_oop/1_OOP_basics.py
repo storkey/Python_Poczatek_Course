93d1d61b556c8eab6af2dc35431b4a6ff9137b75
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-09-22 11:30:57 +0200

    first steps with classes

---
+++ Examples/OOP_I/basics_oop/1_OOP_basics.py
@@ -0,0 +1,24 @@
+class Student:
+    pass
+
+
+class Grade:
+    pass
+
+
+class School:
+    pass
+
+
+if __name__ == "__main__":
+    student_piotr = Student()
+    my_grade = Grade()
+    primary_school = School()
+
+    print(student_piotr)
+    print(my_grade)
+    print(primary_school)
+
+    print("Type of student_piotr object:", type(student_piotr))
+    print("Type of my_grade object:", type(my_grade))
+    print("Type of primary_school object:", type(primary_school))
