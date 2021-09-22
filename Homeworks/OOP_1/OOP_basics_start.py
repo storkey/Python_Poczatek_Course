970470a997eb65cd26676d3b9175e88033ed84ca
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-09-22 13:09:37 +0200

    added content of the task

---
+++ Homeworks/OOP_1/OOP_basics_start.py
@@ -0,0 +1,8 @@
+"""
+    Utwórz klasy do reprezentacji Produktu, Zamówienia, Jabłek i Ziemniaków.
+
+    Stwórz po kilka obiektów typu jabłko i ziemniak i wypisz ich typ za pomocą funkcji wbudowanej type.
+
+    Stwórz listę zawierającą 5 zamówień oraz słownik, w którym kluczami będą nazwy produktów,
+    a wartościami instancje klasy produkt.
+"""
