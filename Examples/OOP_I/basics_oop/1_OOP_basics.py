f534b0fe3804c2aa46b84488e9084761e92abba5
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-09-25 16:38:13 +0200

    added functions, modified fields in Student class

--- Examples/OOP_I/basics_oop/1_OOP_basics.py
+++ Examples/OOP_I/basics_oop/1_OOP_basics.py
@@ -1,10 +1,11 @@
 class Student:
 
     # Konstrukt zostanie wywołany podczas tworzenia obiektu
-    def __init__(self):
-        self.first_name = "Jan"
-        self.last_name = "Piotrowski"
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = 63
+        self.promoted = False
 
 
 class Grade:
@@ -15,21 +16,35 @@ class School:
     pass
 
 
-def run_example():
-    student_jan = Student()
+# Obiekty możemy przekazywać jako argumenty do funkcji
+def print_student(student):
+    print(f"Student: {student.first_name} {student.last_name}, promoted: {student.promoted}")
 
-    print(student_jan.first_name)
-    print(student_jan.last_name)
-    print(student_jan.age)
-    
+
+# W funkcji możemy zmodyfikować stan obiektu (side effect)
+def promote_student(student):
+    student.promoted = True
+
+
+def run_example():
+    student = Student(first_name="Jan", last_name="Kowalski")
+
+    print(student.first_name)
+    print(student.last_name)
+    print(student.age)
+
+    print_student(student)
+    promote_student(student)
+    print_student(student)
+
     # my_grade = Grade()
     # primary_school = School()
     #
-    # print(student_jan)
+    # print(student)
     # print(my_grade)
     # print(primary_school)
     #
-    # print("Type of student_piotr object:", type(student_jan))
+    # print("Type of student_piotr object:", type(student))
     # print("Type of my_grade object:", type(my_grade))
     # print("Type of primary_school object:", type(primary_school))
 
