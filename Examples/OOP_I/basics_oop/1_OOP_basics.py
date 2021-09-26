e65011d04934632b519bb945fb8c5d0e96c4fdfd
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-09-27 01:23:50 +0200

    modified run_example() function

--- Examples/OOP_I/basics_oop/1_OOP_basics.py
+++ Examples/OOP_I/basics_oop/1_OOP_basics.py
@@ -47,26 +47,10 @@ def create_school_with_students(school_name):
 
 
 def run_example():
-    student = Student(first_name="Jan", last_name="Kowalski")
-
-    print(student.first_name)
-    print(student.last_name)
-    print(student.age)
-
-    print_student(student)
-    promote_student(student)
-    print_student(student)
-
-    # my_grade = Grade()
-    # primary_school = School()
-    #
-    # print(student)
-    # print(my_grade)
-    # print(primary_school)
-    #
-    # print("Type of student_piotr object:", type(student))
-    # print("Type of my_grade object:", type(my_grade))
-    # print("Type of primary_school object:", type(primary_school))
+    school = create_school_with_students("Eton")
+    print(school)
+    for student in school.students:
+        print_student(student)
 
 
 if __name__ == "__main__":
