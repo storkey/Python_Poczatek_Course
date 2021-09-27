19ae4417def52e64e2d352679dd8d90ea685bdc6
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-09-28 01:30:13 +0200

    added constructors to Product and Order

--- Homeworks/OOP_1/OOP_basics/classes.py
+++ Homeworks/OOP_1/OOP_basics/classes.py
@@ -1,9 +1,25 @@
 class Product:
-    pass
+
+    def __init__(self, product_name, category_name, price_per_item):
+        self.product_name = product_name
+        self.category_name = category_name
+        self.price_per_item = price_per_item
 
 
 class Order:
-    pass
+
+    def __init__(self, first_name, last_name, products=None):
+        self.first_name = first_name
+        self.last_name = last_name
+
+        if products is None:
+            products = []
+        self.products = products
+
+        total_price = 0
+        for product in products:
+            total_price += product.price_per_item
+        self.total_price = total_price
 
 
 class Apple:
