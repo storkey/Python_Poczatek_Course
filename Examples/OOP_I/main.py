e11b26e5407620580d9f8539287b0e13ec887015
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-10-07 22:29:07 +0200

    cleared main.py file

--- Examples/OOP_I/main.py
+++ Examples/OOP_I/main.py
@@ -1,22 +1,9 @@
-from OOP_I.estudent.school import create_school_with_students, School
-from OOP_I.estudent.student import Student
-
-
-# Ponowny side effect
-def assign_student_to_school(school, student):
-    school.students.append(student)
+from OOP_I.estudent.school import create_school_with_students
 
 
 def run_example():
-    school = create_school_with_students("Eton")
-    school_without_students = School("Empty school")
-    john_doe = Student("John", 'Doe')
-    assign_student_to_school(school_without_students, john_doe)
-    for student in school_without_students.students:
-        student.print_student()
-    print(school)
-    for student in school.students:
-        student.print_student()
+    school = create_school_with_students("Hogwart")
+    school.print_self()
 
 
 if __name__ == "__main__":
