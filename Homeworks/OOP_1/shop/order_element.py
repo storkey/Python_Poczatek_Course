ac9b23f60f2241413d02f95a28f60242c8d55e5f
Piotr Bosianek <piotr.bosianek@gmail.com>
2021-10-09 23:13:52 +0200

    added print_order_element in OrderElement class

--- Homeworks/OOP_1/shop/order_element.py
+++ Homeworks/OOP_1/shop/order_element.py
@@ -6,3 +6,7 @@ class OrderElement:
     def __init__(self, product, quantity):
         self.product = product
         self.quantity = quantity
+
+    def print_order_element(self):
+        self.product.print_product()
+        print(f"\tLiczba sztuk {self.quantity}")
